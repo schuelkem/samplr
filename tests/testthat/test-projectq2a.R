da5386b75faa5d27bf110987c3cbc9560346500f
Matthew Schuelke <matthew.schuelke@slu.edu>
2018-11-28 08:47:05 -0600

    initial unit tests

---
+++ tests/testthat/test-projectq2a.R
@@ -0,0 +1,103 @@
+context("test-projectq2a")
+library(samplr)
+
+test_that("projectq2a returns a numeric vector", {
+  u <- projectq2a(
+    n = 1,
+    pdf = dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+
+  expect_is(object = u, class = "numeric")
+
+  expect_true(object = is.vector(u))
+})
+
+test_that("projectq2a returns correct length", {
+  # n = 1
+  u1 <- projectq2a(
+    n = 1,
+    pdf = dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+
+  expect_true(object = length(u1) == 1)
+
+  # n = 11
+  u11 <- projectq2a(
+    n = 11,
+    pdf = dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+  expect_true(object = length(u11) == 11)
+
+  # n = 111
+  u111 <- projectq2a(
+    n = 111,
+    pdf = dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+
+  expect_true(object = length(u111) == 111)
+})
+
+test_that("projectq2a passes additional parameters to the pdf", {
+  # uniform
+  u <- projectq2a(
+    n = 1,
+    pdf = dunif,
+    a = 0,
+    b = 10,
+    C = 1 / 10,
+    min = 0,
+    max = 10
+  )
+
+  d <- dunif(x = u,
+             min = 0,
+             max = 10)
+
+  expect_true(object = d == 1 / 10)
+
+  # beta
+  b <- projectq2a(
+    n = 1,
+    pdf = dbeta,
+    a = 0,
+    b = 1,
+    C = 1.5,
+    shape1 = 2,
+    shape2 = 2
+  )
+
+  d <- dbeta(x = b,
+             shape1 = 2,
+             shape2 = 2)
+
+  expect_true(object = 0 <= d && d <= 1.5)
+})
+
+test_that("projectq2a returns same as last time", {
+  set.seed(8675309)
+
+  b <- projectq2a(
+    n = 10,
+    pdf = dbeta,
+    a = 0,
+    b = 1,
+    C = 1.5,
+    shape1 = 2,
+    shape2 = 2
+  )
+
+  expect_equal_to_reference(object = b,
+                            file = "test-projectq2a.ref")
+})
---
+++ tests/testthat/test-projectq3a.R
@@ -0,0 +1,121 @@
+context("test-projectq3a")
+library(samplr)
+
+d2dunif <- function(x, y, min = 0, max = 1) {
+  if(min <= x && x <= max && min <= y && y <= max)
+    (max - min)^(-2)
+  else
+    0
+}
+
+d2dbeta <- function(x, y, shape1, shape2) {
+  if(0 <= x && x <= 1 && 0 <= y && y <= 1)
+    (dbeta(x = x, shape1 = shape1, shape2 = shape2) +
+       dbeta(x = y, shape1 = shape1, shape2 = shape2)) / 2
+  else
+    0
+}
+
+test_that("projectq3a returns a data.frame", {
+  u <- projectq3a(
+    n = 1,
+    jpdf = d2dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+
+  expect_is(object = u, class = "data.frame")
+})
+
+test_that("projectq3a returns correct dimensions", {
+  # n = 1
+  u1 <- projectq3a(
+    n = 1,
+    jpdf = d2dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+
+  expect_true(object = nrow(u1) == 1)
+  expect_true(object = ncol(u1) == 2)
+
+  # n = 11
+  u11 <- projectq3a(
+    n = 11,
+    jpdf = d2dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+  expect_true(object = nrow(u11) == 11)
+  expect_true(object = ncol(u11) == 2)
+
+  # n = 111
+  u111 <- projectq3a(
+    n = 111,
+    jpdf = d2dunif,
+    a = 0,
+    b = 1,
+    C = 1
+  )
+
+  expect_true(object = nrow(u111) == 111)
+  expect_true(object = ncol(u111) == 2)
+})
+
+test_that("projectq3a passes additional parameters to the jpdf", {
+  # uniform
+  u <- projectq3a(
+    n = 1,
+    jpdf = d2dunif,
+    a = 0,
+    b = 10,
+    C = 1 / 10,
+    min = 0,
+    max = 10
+  )
+
+  d <- d2dunif(x = u[1, 1],
+               y = u[1, 2],
+               min = 0,
+               max = 10)
+
+  expect_true(object = d == 1 / (10 - 0)^2)
+
+  # beta
+  b <- projectq3a(
+    n = 1,
+    jpdf = d2dbeta,
+    a = 0,
+    b = 1,
+    C = 1.5,
+    shape1 = 2,
+    shape2 = 2
+  )
+
+  d <- dbeta(x = b[1, 1],
+             y = b[1, 2],
+             shape1 = 2,
+             shape2 = 2)
+
+  expect_true(object = 0 <= d && d <= 1.5)
+})
+
+test_that("projectq3a returns same as last time", {
+  set.seed(8675309)
+
+  b <- projectq3a(
+    n = 10,
+    jpdf = d2dbeta,
+    a = 0,
+    b = 1,
+    C = 1.5,
+    shape1 = 2,
+    shape2 = 2
+  )
+
+  expect_equal_to_reference(object = b,
+                            file = "test-projectq3a.ref")
+})
